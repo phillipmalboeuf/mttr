eab5422313c26835fd1358f7265b968f45524bb7
Phillip Malboeuf <phil@phils.computer>
2021-09-23 13:30:24 -0400

    adapter next

--- src/lib/clients/contentful.ts
+++ src/lib/clients/contentful.ts
@@ -1,6 +1,6 @@
-import { createClient } from 'contentful'
-// import pkg from 'contentful'
-// const { createClient } = pkg
+// import { createClient } from 'contentful'
+import pkg from 'contentful'
+const { createClient } = pkg
 
 export const contentful = createClient({
   space: 'jvkvbm860dig',
